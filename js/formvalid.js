a85ef775a70da45d40db0388d26b0de1e559eb4a
Francielle Garcia <franciellex.garcia@intel.com>
2023-12-08 15:10:33 -0300

    FinalProject

---
+++ js/formvalid.js
@@ -0,0 +1,22 @@
+function handleFormSubmission(event) {
+    event.preventDefault();
+
+    var form = document.getElementById('contactForm');
+    var nameInput = document.getElementById('name1');
+    var emailInput = document.getElementById('email1');
+    var phoneInput = document.getElementById('phone1');
+    var messageInput = document.getElementById('message1');
+
+
+    if (nameInput.validity.valid && emailInput.validity.valid && phoneInput.validity.valid && messageInput.validity.valid) {
+        alert('Message successfully sent!');
+        form.reset();
+    } else {
+        alert('Please fill out all required fields correctly.');
+    }
+}
+    function handleFormSubmission(event) {
+        event.preventDefault();
+        alert('Message successfully sent!');
+        document.getElementById('contactForm').reset();
+    }
---
+++ js/script.js
@@ -0,0 +1,16 @@
+let cartItemCount = 0;
+
+function addToCart() {
+    // Update the cart count
+    cartItemCount++;
+    
+    // Update the display
+    updateCartDisplay();
+}
+
+function updateCartDisplay() {
+    // Update the cart count in the HTML
+    const cartItemCountElement = document.getElementById('cartItemCount');
+    cartItemCountElement.textContent = cartItemCount;
+}
+
